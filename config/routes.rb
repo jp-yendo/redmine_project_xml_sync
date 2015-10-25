cbd14dfcc80a363369c01ace37b35ed112624377
yendo <yendo@localhost.localdomain>
2015-10-25 22:30:53 +0900

    code update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 RedmineApp::Application.routes.draw do
-#  match 'project_xml_sync/(:action(/:id))', :controller => 'project_xml_sync', :via => [:get, :post], as: 'project_xml_sync_route'
-#  match 'project_xml_sync/:action/:id', :to => 'project_xml_sync#show', :via => [:get, :post], as: 'project_xml_sync_route_import'
   match 'project_xml_sync/(:action(/:id))', :controller => 'project_xml_sync', :via => [:get, :post], as: 'project_xml_sync_route'
 end
